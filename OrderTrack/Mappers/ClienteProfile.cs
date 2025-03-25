3586bc9db60f333e8ac470408272e1a6d2a2782e
AlvaroGomez <alvaro.gomez@oudos-it.com>
2025-03-25 09:28:26 -0500

    Creacion de mappers

---
+++ OrderTrack/Mappers/ClienteProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OrderTrack.Models.Domain;
+using OrderTrack.Models.DTO;
+
+namespace OrderTrack.Mappers
+{
+    public class ClienteProfile:Profile
+    {
+        public ClienteProfile()
+        {
+            CreateMap<ClienteDTO, Cliente>();
+        }
+    }
+}
---
+++ OrderTrack/Mappers/ProductoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OrderTrack.Models.Domain;
+using OrderTrack.Models.DTO;
+
+namespace OrderTrack.Mappers
+{
+    public class ProductoProfile:Profile
+    {
+        public ProductoProfile()
+        {
+            CreateMap<ProductoDTO, Producto>();
+        }
+    }
+}
