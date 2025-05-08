012399d38574662716a969f7a481e6c7112aac8c
AlvaroG1016 <alvaro.g.b@outlook.com>
2025-05-07 20:35:18 -0500

    Add dockerfile

--- OrderTrack/Program.cs
+++ OrderTrack/Program.cs
@@ -52,11 +52,10 @@ builder.Services.AddTransient<IDatatableService, DatatableService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
